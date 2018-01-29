ccf1673b34cdcf489f60616c75211a0841f10962
nam.dng2511 <nam.dng2511@gmail.com>
2018-01-29 22:30:36 +0700

    implement server in one container connect to the database in one other container

---
+++ application/app/Http/Controllers/PagesController.php
@@ -0,0 +1,11 @@
+<?php 
+namespace App\Http\Controllers;
+class PagesController extends Controller {
+	public function getIndex() {
+		return view('pages.welcome');
+	}
+	
+	public function getContact() {
+		return view('pages.contact');
+	}
+}
---
+++ application/resources/views/partials/_footer.blade.php
@@ -0,0 +1,3 @@
+<hr>
+
+<p class="text-center">Copyright Group 2 ICT K59 - All Rights Reserved</p>
