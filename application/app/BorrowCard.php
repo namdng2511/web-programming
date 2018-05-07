affcad7965a45dc9d45b03642168649a694ed495
ltthacker <20144075@student.hust.edu.vn>
2018-05-07 16:32:18 +0700

    user and borrow card usecase

---
+++ application/app/BorrowCard.php
@@ -0,0 +1,63 @@
+<?php
+namespace App;
+use DateTime;
+use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
+class BorrowCard extends Model
+{
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+    protected $table = 'borrow_cards';
+    /**
+     * Retrieve the borrow card that have the card number in the database
+     * @param $cardNumber
+     * @return BorrowCard
+     */
+    public function getBorrowCardByCardNumber($cardNumber)
+    {
+        return $this->where('card_number', $cardNumber)->first();
+    }
+    /**
+     * Check the borrowing card belongs to the current user
+     * @param $userID
+     * @return boolean
+     */
+    public function validateBorrowCard($userID)
+    {
+        return $this->getAttribute('user')->getAttribute('id') == $userID;
+    }
+    /**
+     * Check if the borrow card is expired
+     * @return boolean
+     */
+    public function checkExpirationDate()
+    {
+        return new DateTime($this->getAttribute('expired_date')) > new DateTime();
+    }
+    public function createNewBorrowCard($userID){
+        $this->setAttribute('card_number', self::generateBorrowCardNumber())
+            ->setAttribute('user_id', $userID)
+            ->setAttribute('activation_code', self::generateRandomActivationCode())
+            ->setAttribute('expired_date', Carbon::now()->addYears(2))
+            ->setAttribute('is_activated', false);
+        
+        $this->save();
+    }
+    private function generateBorrowCardNumber() {
+        $noOfBorrowCard = $this->count();
+        $sequenceNumber = $noOfBorrowCard + 1;
+        return str_pad($sequenceNumber , 4, "0", STR_PAD_LEFT );
+    }
+    private function generateRandomActivationCode($length = 10) {
+        return substr(str_shuffle("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"), 0, $length);
+    }
+    public function isActivated()
+    {
+        return $this->getAttribute('is_activated');
+    }
+}
--- application/app/User.php
+++ application/app/User.php
@@ -1,23 +1,21 @@
 <?php
-
 namespace App;
-
+use DateTime;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-
+use Zizaco\Entrust\Traits\EntrustUserTrait;
 class User extends Authenticatable
 {
     use Notifiable;
-
+    use EntrustUserTrait;
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'full_name', 'gender', 'contact', 'is_student', 'student_id', 'expired_at',
     ];
-
     /**
      * The attributes that should be hidden for arrays.
      *
@@ -26,4 +24,65 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+    /**
+     * Get number of borrowed copies by the user
+     * @return integer
+     */
+    public function getNumberOfBorrowedCopies(){
+        return (new BorrowRecord())->where('user_id', $this->getAttribute('id'))
+            ->whereIn('status', ['registered', 'accepted', 'lent'])->count();
+    }
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function borrowCards()
+    {
+        return $this->hasMany('App\BorrowCard');
+    }
+    /**
+     * Get active borrow card
+     * @return \Illuminate\Database\Eloquent\Model|null|static
+     */
+    public function getActiveBorrowCard()
+    {
+        return $this->borrowCards()->where('expired_date', '>', new DateTime())
+            ->first();
+    }
+    /**
+     * Find user by user name
+     * @param string $username
+     * @return mixed
+     */
+    public function getUserByUsername($username)
+    {
+        return $this->where('name', $username)->first();
+    }
+    public function getUserByUserID($userID)
+    {
+        return $this->where('id', $userID)->first();
+    }
+    /**
+     * Get active card number
+     * @return mixed|null
+     */
+    public function getBorrowCardNumber()
+    {
+        if (is_null($this->getActiveBorrowCard())) {
+            return null;
+        }
+        return $this->getActiveBorrowCard()->getAttribute('card_number');
+    }
+    public function borrowRecords()
+    {
+        return $this->hasMany('App\BorrowRecord');
+    }
+    public function hasOverDueBorrow()
+    {
+        foreach ($this->getAttribute('borrowRecords') as $record) {
+            if ($record->isOverDue()) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
