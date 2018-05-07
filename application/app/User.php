66fe615f63100125776a205d6ccfbc455e21de02
ngocpham <ngocpham0501@gmail.com>
2018-05-07 21:18:40 +0700

    Add controller, view and modify model user for LogIn

--- application/app/User.php
+++ application/app/User.php
@@ -1,21 +1,26 @@
 <?php
+
 namespace App;
+
 use DateTime;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Zizaco\Entrust\Traits\EntrustUserTrait;
+
 class User extends Authenticatable
 {
     use Notifiable;
     use EntrustUserTrait;
+
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'full_name', 'gender', 'contact', 'is_student', 'student_id', 'expired_at',
+        'name', 'email', 'password',
     ];
+
     /**
      * The attributes that should be hidden for arrays.
      *
@@ -24,6 +29,7 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
     /**
      * Get number of borrowed copies by the user
      * @return integer
@@ -32,6 +38,7 @@ class User extends Authenticatable
         return (new BorrowRecord())->where('user_id', $this->getAttribute('id'))
             ->whereIn('status', ['registered', 'accepted', 'lent'])->count();
     }
+
     /**
      * @return \Illuminate\Database\Eloquent\Relations\HasMany
      */
@@ -39,6 +46,7 @@ class User extends Authenticatable
     {
         return $this->hasMany('App\BorrowCard');
     }
+
     /**
      * Get active borrow card
      * @return \Illuminate\Database\Eloquent\Model|null|static
@@ -48,6 +56,7 @@ class User extends Authenticatable
         return $this->borrowCards()->where('expired_date', '>', new DateTime())
             ->first();
     }
+
     /**
      * Find user by user name
      * @param string $username
@@ -57,10 +66,7 @@ class User extends Authenticatable
     {
         return $this->where('name', $username)->first();
     }
-    public function getUserByUserID($userID)
-    {
-        return $this->where('id', $userID)->first();
-    }
+
     /**
      * Get active card number
      * @return mixed|null
@@ -70,19 +76,7 @@ class User extends Authenticatable
         if (is_null($this->getActiveBorrowCard())) {
             return null;
         }
+
         return $this->getActiveBorrowCard()->getAttribute('card_number');
-    }
-    public function borrowRecords()
-    {
-        return $this->hasMany('App\BorrowRecord');
-    }
-    public function hasOverDueBorrow()
-    {
-        foreach ($this->getAttribute('borrowRecords') as $record) {
-            if ($record->isOverDue()) {
-                return true;
-            }
-        }
-        return false;
     }
 }
